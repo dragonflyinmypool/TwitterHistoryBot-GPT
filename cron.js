737223251276d2e3bae2163401c0d40efdc02b9b
dragonflyinmypool <jheiney10@gmail.com>
2022-12-12 16:22:54 -0500

    cron back to ususal

--- cron.js
+++ cron.js
@@ -8,13 +8,13 @@ const getTweet = require("./index.js");
 var cron = require("node-cron");
 
 // cron job three times a day 8am 12 noon and 6pm
-// cron.schedule("0 8,12,18 * * *", () => {
-//   console.log("running task at specific time");
-//   getTweet.getNewTeet();
-// });
-
-// cron job every min
-cron.schedule("* * * * * *", () => {
-  console.log("running task every min");
+cron.schedule("0 8,12,18 * * *", () => {
+  console.log("running task at specific time");
   getTweet.getNewTeet();
 });
+
+// // cron job every min
+// cron.schedule("* * * * * *", () => {
+//   console.log("running task every min");
+//   getTweet.getNewTeet();
+// });
