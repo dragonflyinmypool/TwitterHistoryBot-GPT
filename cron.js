61b6766188308df4763ad52730aaae68dd62ea34
dragonflyinmypool <jheiney10@gmail.com>
2022-12-18 13:09:26 -0500

    "regular times"

--- cron.js
+++ cron.js
@@ -7,14 +7,14 @@ const getTweet = require("./index.js");
 
 var cron = require("node-cron");
 
-// // cron job three times a day 8am 12 noon and 6pm
-// cron.schedule("0 8,12,18 * * *", () => {
-//   console.log("running task at specific time");
-//   getTweet.getNewTeet();
-// });
-
-// cron job for every hour
-cron.schedule("* * * * * *", () => {
-  console.log("running task every min");
+// cron job three times a day 8am 12 noon and 6pm
+cron.schedule("0 8,12,18 * * *", () => {
+  console.log("running task at specific time");
   getTweet.getNewTeet();
 });
+
+// // cron job for every hour
+// cron.schedule("* * * * * *", () => {
+//   console.log("running task every min");
+//   getTweet.getNewTeet();
+// });
