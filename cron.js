8eb33b56b27fb6fc89e14729900e368687b34040
dragonflyinmypool <jheiney10@gmail.com>
2022-12-18 13:03:09 -0500

    "testing every min"

--- cron.js
+++ cron.js
@@ -7,8 +7,13 @@ const getTweet = require("./index.js");
 
 var cron = require("node-cron");
 
-// cron job three times a day 8am 12 noon and 6pm
-cron.schedule("0 8,12,18 * * *", () => {
-  console.log("running task at specific time");
-  getTweet.getNewTeet();
+// // cron job three times a day 8am 12 noon and 6pm
+// cron.schedule("0 8,12,18 * * *", () => {
+//   console.log("running task at specific time");
+//   getTweet.getNewTeet();
+// });
+
+// cron job for every hour
+cron.schedule("*/1 * * * *", () => {
+  console.log("running task every hour");
 });
