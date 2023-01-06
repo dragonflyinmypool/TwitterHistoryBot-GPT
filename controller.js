1c938bce69f46582f7ed0a2f2b93c71a43143fa2
dragonflyinmypool <jheiney10@gmail.com>
2023-01-06 10:59:31 -0500

    scrapper blocked

--- controller.js
+++ controller.js
@@ -34,8 +34,8 @@ const generalBot = async () => {
 // cron job at 5:22pm every day
 
 // cron at 6pm every day
-const timing = '0 22 17 * * *';
-const name = '6pm once a day';
-// generalBot();
-scheduleTask('5:50pm once a day', timing, generalBot);
-console.log(timing);
+// const timing = '0 22 17 * * *';
+// const name = '6pm once a day';
+generalBot();
+// scheduleTask('5:50pm once a day', timing, generalBot);
+// console.log(timing);
